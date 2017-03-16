9da77ceb4276352463848a8436e0dbcbea936649
mbarberot <mathieu.barberot@gmail.com>
2017-03-16 21:31:24 +0100

    Styling : using semantic ui and sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,19 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: ['bower_components/semantic-ui-sass/app/assets/stylesheets']
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  return app.toTree();
+  var cssAssets = new Funnel('bower_components/semantic-ui-sass/app/assets/fonts/semantic-ui', {
+    srcDir: '/',
+    include: ['**/*.*'],
+    destDir: '/assets/fonts'
+  });
+  return app.toTree(new MergeTrees([cssAssets]));
 };
