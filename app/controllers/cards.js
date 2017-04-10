3edadf373894da845e8b528f3011a9bdca8f58a8
Mathieu Barberot <mathieu.barberot@skin-soft.org>
2017-04-10 15:27:07 +0200

    Implements pagination

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,16 +1,68 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  meta: Ember.computed('meta', {
+  pagination: Ember.computed('model.meta', {
     get() {
-      console.log(this.get('model.meta'));
-      return this.get('model.meta');
+      let meta = this.get('model.meta');
+      let pageCount = parseInt(meta.total / meta.size) + (meta.total % meta.size !== 0 ? 1 : 0);
+      let desiredSize = (pageCount < 5 ? pageCount : 5);
+      let currentPage = meta.current_page;
+      let leftIndex = 0;
+      let rightIndex = 0;
+
+      let pagination = [{
+        'isActive': true,
+        'page': `${currentPage}`,
+        'content': `${currentPage}`
+      }];
+
+      while (pagination.length < desiredSize) {
+        leftIndex--;
+        let leftPage = currentPage + leftIndex;
+        if (leftPage > 0) {
+          pagination.splice(0, 0, {
+            'page': `${leftPage}`,
+            'content': `${leftPage}`
+          });
+        }
+
+        rightIndex++;
+        let rightPage = currentPage + rightIndex;
+        if (rightPage <= pageCount) {
+          pagination.push({
+            'page': `${rightPage}`,
+            'content': `${rightPage}`
+          });
+        }
+      }
+
+      pagination.splice(0, 0, {
+        'hasIcon': true,
+        'page': '1',
+        'content': Ember.String.htmlSafe('<i class="step backward icon"></i>')
+      });
+      pagination.splice(pagination.length, 0, {
+        'hasIcon': true,
+        'page': `${pageCount}`,
+        'content': Ember.String.htmlSafe('<i class="step forward icon"></i>')
+      });
+
+      return pagination;
     }
   }),
 
   actions: {
+    page(pageNumber) {
+      this.set('page', pageNumber);
+    },
     search() {
-      this.model(this.getProperties('query', 'page', 'size'));
+      this.transitionToRoute('cards', {
+        queryParams: {
+          'query': this.get('queryInput'),
+          'page': 1,
+          'size': this.get('size')
+        }
+      });
     }
   }
 });
